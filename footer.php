a5cde52af4ba142f449452c4b7917e761ec9524f
Navid Vafaei <vafaermn@gmail.com>
2024-11-13 22:06:51 +0330

    some change

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <h1 style="padding:50px 0;">Navid</h1>
+        <p>همه حق نشر © این وبسایت تحت نوید وفایی محفوظ است.</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
